471f50c4cb083a4862f125a69cba846b0ad1c744
dev4lex <dev4lex@gmail.com>
2020-05-22 01:19:22 +0300

    new step in lesson 2

---
+++ Selenium/2.1_step7.py
@@ -0,0 +1,34 @@
+from selenium import webdriver
+import time
+import math
+
+def calc(x):
+  return str(math.log(abs(12*math.sin(int(x)))))
+
+try:
+    link = "http://suninjuly.github.io/get_attribute.html"
+    browser = webdriver.Chrome()
+    browser.get(link)
+
+    x_element = browser.find_element_by_id("treasure")
+    x = x_element.get_attribute('valuex')
+    y = calc(x)
+
+    input1 = browser.find_element_by_id("answer")
+    input1.send_keys(y)
+
+    option = browser.find_element_by_id("robotCheckbox")
+    option.click()
+
+    option1 = browser.find_element_by_id("robotsRule")
+    option1.click()
+
+    option2 = browser.find_element_by_class_name("btn-default")
+    option2.click()
+
+
+finally:
+    # ожидание чтобы визуально оценить результаты прохождения скрипта
+    time.sleep(10)
+    # закрываем браузер после всех манипуляций
+    browser.quit()
