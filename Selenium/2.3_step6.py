7b2449cbca67101fc45e7b7f98cdff1a4442bba3
dev4lex <dev4lex@gmail.com>
2020-05-23 02:33:14 +0300

    add lesson 2.3 step 6

---
+++ Selenium/2.3_step6.py
@@ -0,0 +1,36 @@
+from selenium import webdriver
+import time
+import math
+
+def calc(x):
+  return str(math.log(abs(12*math.sin(int(x)))))
+
+
+try:
+    link = "http://suninjuly.github.io/redirect_accept.html"
+    browser = webdriver.Chrome()
+    browser.get(link)
+
+    option1 = browser.find_element_by_class_name("btn-primary")
+    option1.click()
+
+    new_window = browser.window_handles[1]
+    first_window = browser.window_handles[0]
+
+    browser.switch_to.window(new_window)
+    x_element = browser.find_element_by_id("input_value")
+    x = x_element.text
+    y = calc(x)
+
+    input1 = browser.find_element_by_id("answer")
+    input1.send_keys(y)
+
+    option2 = browser.find_element_by_class_name("btn-primary")
+    option2.click()
+
+
+finally:
+    # ожидание чтобы визуально оценить результаты прохождения скрипта
+    time.sleep(10)
+    # закрываем браузер после всех манипуляций
+    browser.quit()
